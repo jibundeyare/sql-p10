0b7ee9b67c81eb9051b13630cb729263af69802a
jibundeyare <contact@jibundeyare.com>
2023-01-30 22:02:36 +0100

    Améliorations

--- blog-structure.sql
+++ blog-structure.sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 30 jan. 2023 à 14:52
+-- Généré le : lun. 30 jan. 2023 à 22:00
 -- Version du serveur : 10.3.37-MariaDB-0ubuntu0.20.04.1
 -- Version de PHP : 8.1.13
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -20,6 +21,21 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `blog`
 --
+CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `blog`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -27,12 +43,14 @@ SET time_zone = "+00:00";
 -- Structure de la table `post`
 --
 
+DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `id` int(10) UNSIGNED NOT NULL,
   `subject` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `publication_date` datetime DEFAULT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -41,6 +59,7 @@ CREATE TABLE `post` (
 -- Structure de la table `post_tag`
 --
 
+DROP TABLE IF EXISTS `post_tag`;
 CREATE TABLE `post_tag` (
   `post_id` int(10) UNSIGNED NOT NULL,
   `tag_id` int(10) UNSIGNED NOT NULL
@@ -52,6 +71,7 @@ CREATE TABLE `post_tag` (
 -- Structure de la table `profil`
 --
 
+DROP TABLE IF EXISTS `profil`;
 CREATE TABLE `profil` (
   `id` int(10) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL,
@@ -66,6 +86,7 @@ CREATE TABLE `profil` (
 -- Structure de la table `role`
 --
 
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` int(10) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL
@@ -77,6 +98,7 @@ CREATE TABLE `role` (
 -- Structure de la table `role_user`
 --
 
+DROP TABLE IF EXISTS `role_user`;
 CREATE TABLE `role_user` (
   `role_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL
@@ -88,6 +110,7 @@ CREATE TABLE `role_user` (
 -- Structure de la table `tag`
 --
 
+DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag` (
   `id` int(10) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL
@@ -99,6 +122,7 @@ CREATE TABLE `tag` (
 -- Structure de la table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -111,11 +135,18 @@ CREATE TABLE `user` (
 --
 
 --
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_post_user_id` (`user_id`);
+  ADD KEY `fk_post_user_id` (`user_id`),
+  ADD KEY `fk_post_category_id` (`category_id`);
 
 --
 -- Index pour la table `post_tag`
@@ -143,7 +174,8 @@ ALTER TABLE `role`
 --
 ALTER TABLE `role_user`
   ADD UNIQUE KEY `idx_role_user_user_id_role_id_unique` (`user_id`,`role_id`),
-  ADD KEY `fk_role_user_role_id` (`role_id`);
+  ADD KEY `fk_role_user_role_id` (`role_id`),
+  ADD KEY `idx_role_user_user_id` (`user_id`);
 
 --
 -- Index pour la table `tag`
@@ -162,6 +194,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `post`
@@ -201,6 +239,7 @@ ALTER TABLE `user`
 -- Contraintes pour la table `post`
 --
 ALTER TABLE `post`
+  ADD CONSTRAINT `fk_post_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
   ADD CONSTRAINT `fk_post_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
@@ -222,6 +261,7 @@ ALTER TABLE `profil`
 ALTER TABLE `role_user`
   ADD CONSTRAINT `fk_role_user_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   ADD CONSTRAINT `fk_role_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
